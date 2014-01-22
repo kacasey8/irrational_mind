2293aea2cda375c133b14c280397fb825f4090ee
Kevin Casey <kacasey@berkeley.edu>
2014-01-21 20:02:46 -0800

    vanilla rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IrrationalMind::Application.config.secret_key_base = '5781ab3ff14fb2c8c570747eb6c2a7af2bb73032d6ee518b9a5fadb69900d6e0dc1108b86cd28b137e88305cadefd3ddc6f025540aee97aecbde92c388ac6e90'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+IrrationalMind::Application.config.session_store :cookie_store, key: '_irrational_mind_session'
